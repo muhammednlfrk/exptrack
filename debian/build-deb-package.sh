e343379eebc0e4d31119a146ce47b0dc889c517a
Muhammed Nilifırka <41184896+muhammednlfrk@users.noreply.github.com>
2025-03-09 03:31:35 +0300

    debian package files moved

--- debian/build-deb-package.sh
+++ debian/build-deb-package.sh
@@ -2,9 +2,9 @@
 
 VERSION="v1.0.0"
 OUTPUT_FILE_NAME="exptrack-linux-x64-$VERSION.deb"
-mkdir --parents --verbose DEBIAN/usr/bin/
+mkdir --parents --verbose usr/bin/
 echo "Copying package files..."
-cp ./publish/linux-x64/exptrack ./DEBIAN/usr/bin/exptrack
+cp ../publish/linux-x64/exptrack ./usr/bin/exptrack
 echo "Creating debian package..."
 dpkg-deb --build . $OUTPUT_FILE_NAME
 echo "Done!"
